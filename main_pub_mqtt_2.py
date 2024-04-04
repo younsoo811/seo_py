464dc56f70a0097543267ee21924995a2993c6fe
younsoo <younsoo7795@gmail.com>
2024-04-04 13:20:00 +0900

    240404

--- main_pub_mqtt_2.py
+++ main_pub_mqtt_2.py
@@ -9,13 +9,25 @@ from tkinter.ttk import *
 import tkinter.messagebox
 import tkinter.font
 
+entry_list=[]
+with open('_internal/data/entry_data.txt', 'r') as file:
+    for i in file:
+        entry_list.append(i.strip())
+
 with open('framedata.json', 'r') as json_file:
     framedata_json=json.load(json_file)
 with open('event.json', 'r') as json_file:
     event_json=json.load(json_file)
 
 count=1
+check_connect=False
+check_disconnect=False
+
 def pub():
+    global check_connect
+    global check_disconnect
+    global entry_list
+
     entry_pf=profile_entry.get()
     entry_cam=camera_id_entry.get()
 
@@ -24,17 +36,40 @@ def pub():
 
     entry_time = time_entry.get()
     entry_size = size_entry.get()
+
+    # 나중에 entry 값 리스트로 저장시 for문으로 돌리기
+    entry_list[0]=entry_pf
+    entry_list[1]=entry_cam
+    entry_list[2]=entry_pfid
+    entry_list[3]=entry_add
+    entry_list[4]=entry_time
+    entry_list[5]=entry_size
+
     main_label['text']="topic당 "+str(entry_size)+"개 전송 ("+str(entry_time)+"s)"
+    #check_label['text']="success"
+    if entry_pf=="":
+        entry_pf="profile"
+    if entry_cam=="":
+        entry_cam="camera_id"
+    if entry_pfid==None:
+        entry_pfid=2
+    if entry_add=="":
+        entry_add="address"
 
     def on_connect(client, userdata, flags, rc):
+        global check_connect
         if rc == 0:
             print("connected OK")
+            check_connect=True
         else:
             print("Bad connection Returned code=", rc)
+            check_connect=False
 
     # 연결 정상적으로 끊어짐 (rc=0)
     def on_disconnect(client, userdata, flags, rc=0):
+        global check_disconnect
         print("disconnect: "+str(rc))
+        check_disconnect=True
 
 
     # 새로운 클라이언트 생성
@@ -43,9 +78,13 @@ def pub():
     client.on_connect = on_connect
     client.on_disconnect = on_disconnect
 
+    client.username_pw_set("seo_ai", "qrqvud3")
     client.connect('localhost', 1883)
 
+    time_save=str(time.strftime("%H: %M: %S", time.localtime(time.time())))
+
     client.loop_start()
+
     global count
     for j in range(0,int(entry_time)):
         start_time=time.time()
@@ -87,6 +126,22 @@ def pub():
     
     client.disconnect()
 
+    # 로그 기록 변경
+    if check_connect==True:      
+        check_label['text']="success"
+        check_time_label['text']=time_save
+        check_connect=False
+
+    if check_disconnect==True:
+        pub_label['text']="finish"
+        pub_time_label['text']=str(time.strftime("%H: %M: %S", time.localtime(time.time())))
+        check_disconnect=False
+    
+    # 엔트리 내용 백업
+    with open('_internal/data/entry_data.txt', 'w+') as f:
+        f.write('\n'.join(entry_list))
+
+
 
 main_window = tkinter.Tk()
 main_window.title("MQTT Publisher")
@@ -102,9 +157,11 @@ sender_frame.pack(side="top", expand=True, padx=5, pady=5)
 tkinter.Label(sender_frame, text="senders:", width=10).grid(row=1, column=0)
 profile_entry=tkinter.Entry(sender_frame, width=20)
 profile_entry.grid(row=1, column= 1, padx=10)
+profile_entry.insert(0, entry_list[0])
 tkinter.Label(sender_frame, text="profile(token)", width=10,anchor=W).grid(row=1, column=2)
 camera_id_entry=tkinter.Entry(sender_frame, width=20)
 camera_id_entry.grid(row=2, column= 1, padx=10, pady=5)
+camera_id_entry.insert(0, entry_list[1])
 tkinter.Label(sender_frame, text="camera_id", width=10,anchor=W).grid(row=2, column=2)
 
 event_frame=LabelFrame(main_window, text="event.json")
@@ -113,11 +170,13 @@ event_frame.pack(side="top", expand=True, padx=5, pady=5)
 tkinter.Label(event_frame, text="profile_id:", width=10).grid(row=4, column=0)
 profile_id_entry=tkinter.Entry(event_frame, width=20)
 profile_id_entry.grid(row=4, column= 1, padx=10, pady=15)
+profile_id_entry.insert(0, entry_list[2])
 tkinter.Label(event_frame, text="(type->int)", width=10,anchor=W).grid(row=4, column=2)
 # address
 tkinter.Label(event_frame, text="address:", width=10).grid(row=5, column=0)
 address_entry=tkinter.Entry(event_frame, width=20)
 address_entry.grid(row=5, column= 1, padx=10, pady=15)
+address_entry.insert(0, entry_list[3])
 
 
 info_frame=LabelFrame(main_window, text="setting")
@@ -138,13 +197,13 @@ event_check.grid(row=6, column= 2, pady=10)
 tkinter.Label(set_frame, text="size:", width=10).grid(row=7, column=0)
 size_entry=tkinter.Entry(set_frame, width=20)
 size_entry.grid(row=7, column= 1, padx=10, pady=10)
-size_entry.insert(0, "1")
+size_entry.insert(0, entry_list[4])
 tkinter.Label(set_frame, text="번 전송", width=10,anchor=W).grid(row=7, column=2)
 # 전송 시간 설정
 tkinter.Label(set_frame, text="time:", width=10).grid(row=8, column=0)
 time_entry=tkinter.Entry(set_frame, width=20)
 time_entry.grid(row=8, column= 1, padx=10)
-time_entry.insert(0,"1")
+time_entry.insert(0,entry_list[5])
 tkinter.Button(set_frame, text="send", width=8, command=pub).grid(row = 8, column = 2, padx = 10)
 
 tkinter.Label(info_frame, text="", width=10).grid(row=6, column=0)
@@ -157,11 +216,16 @@ log_frame=LabelFrame(main_window, text="log")
 log_frame.pack(side="top", expand=True, padx=5, pady=5)
 # 연결 성공 여부
 tkinter.Label(log_frame, text="connect:", width=10, anchor=E, padx=10, pady=5).grid(row=0, column=0)
-check_label=tkinter.Label(log_frame, text="Fail", width=15, anchor=W).grid(row=0, column=1)
+check_label=tkinter.Label(log_frame, text="Fail", width=15, anchor=W)
+check_label.grid(row=0, column=1)
+check_time_label=tkinter.Label(log_frame, text="(time)", width=20, anchor=W)
+check_time_label.grid(row=0, column=2)
 # 전송 성공 여부
 tkinter.Label(log_frame, text="publish:", width=10, anchor=E).grid(row=1, column=0)
-pub_label=tkinter.Label(log_frame, text="None", width=15, anchor=W).grid(row=1, column=1)
-tkinter.Label(log_frame, text="(time)", width=20, anchor=W).grid(row=1, column=2)
+pub_label=tkinter.Label(log_frame, text="None", width=15, anchor=W)
+pub_label.grid(row=1, column=1)
+pub_time_label=tkinter.Label(log_frame, text="(time)", width=20, anchor=W)
+pub_time_label.grid(row=1, column=2)
 
 b_label=tkinter.Label(main_window, text = "", height=2)
 b_label.pack(side="top", expand=True, padx=5, pady=5)
